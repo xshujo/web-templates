0df9d1bbc62d71803ec285e777939c8e72b1442c
Taïsha Dorval-Mbele <137970027+xshujo@users.noreply.github.com>
2023-10-13 13:01:33 -0400

    menu-start-anim-1 - Added comments

--- menu-start-anim-1/script.js
+++ menu-start-anim-1/script.js
@@ -1,3 +1,5 @@
+// The animation only starts when the body has loaded
+// I don't recommend removing this but if you do, remove ".loaded" next to body in style.scss/css
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
